49c967c3f237c46beb5ecf22b9b377f792aa5b4e
janinjan <janin.culhaoglu@gmail.com>
2019-05-01 17:16:15 +0200

    Implemented ViewController class

--- SimpleStopWatch/Controller/ViewController.swift
+++ SimpleStopWatch/Controller/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - Properties
+    var timer = Timer()
+    var runTime: Float = 0.0
+    
+    // MARK: - Outlets
+    @IBOutlet weak var textView: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    // MARK: - Actions
+    @IBAction func reset(_ sender: UIButton) {
+        timer.invalidate()
+        runTime = 0.0
+        updateDisplay()
+    }
+    
+    @IBAction func didTapPlay(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(runTimeAction), userInfo: nil, repeats: true)
+    }
+    
+    @objc func runTimeAction() {
+        runTime += 0.1
+        updateDisplay()
+    }
+    
+    @IBAction func didTapPause(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        AddBorderToLayers()
     }
-
-
+    
+    private func AddBorderToLayers() {
+        playButton.layer.borderWidth = 1.0
+        playButton.layer.borderColor = UIColor.black.cgColor
+        pauseButton.layer.borderWidth = 1.0
+        pauseButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    private func updateDisplay() {
+        textView.text = String(runTime.cleanValue)
+    }
 }
 
+extension Float {
+    var cleanValue: String {
+        return String(format: "%.1f", self)
+    }
+}
