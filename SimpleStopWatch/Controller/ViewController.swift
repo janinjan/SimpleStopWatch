8c426bfa8e794e395f8e86cb931a542b1f790204
janinjan <janin.culhaoglu@gmail.com>
2019-05-01 13:19:46 +0200

    Initial Commit

---
+++ SimpleStopWatch/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleStopWatch
+//
+//  Created by Janin Culhaoglu on 01/05/2019.
+//  Copyright © 2019 Janin Culhaoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
